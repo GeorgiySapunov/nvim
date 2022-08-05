e0dc0a87d3175ab38fb439a7e843c03f04473ea2
Georgiy Sapunov <georgiysapunov@yandex.ru>
2022-08-05 22:31:20 +0300

    catppuccin and illuminate

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,13 @@
-local colorscheme = "nord"
+-- local colorscheme = "nord"
+--
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+-- 	vim.notify("colorscheme " .. colorscheme .. " not found!")
+-- 	return
+-- end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
-end
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+require("catppuccin").setup()
+
+vim.cmd [[colorscheme catppuccin]]
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,7 +62,8 @@ return packer.startup(function(use)
   end
   }
   -- Colorschemes
-  use "rafi/awesome-vim-colorschemes"
+  -- use "rafi/awesome-vim-colorschemes"
+use { "catppuccin/nvim", as = "catppuccin" }
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -80,7 +81,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  -- use "RRethy/vim-illuminate"
+  use "RRethy/vim-illuminate"
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
