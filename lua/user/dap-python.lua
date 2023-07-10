0315f45e3a63ea18fefa4ad27f734b6c807fdc98
Georgiy Sapunov <georgiysapunov@yandex.ru>
2023-07-10 23:24:12 +0800

    dap-python

---
+++ lua/user/dap-python.lua
@@ -0,0 +1,16 @@
+local M = {
+  "mfussenegger/nvim-dap-python",
+  event = "VeryLazy",
+  ft = "python",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+  },
+}
+
+function M.config()
+  local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+  require("dap-python").setup(path)
+end
+
+return M
